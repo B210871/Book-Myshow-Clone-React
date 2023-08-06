d77f75c1e7e5cebd931ef6eb53554d664c313e67
B210871 <gautamabhay81587@gmail.com>
2023-08-06 12:34:26 +0530

    This is my first website using carousal

--- src/about.js
+++ src/about.js
@@ -4,3 +4,8 @@ const About =()=>{
     return <div>About component</div>
 }
 export default About;
+
+
+
+
+<div class="sc-qswwm9-1 fDYgzI" , url(&quot;https://assets-in.bmscdn.com/iedb/movies/images/mobile/listing/xxlarge/omg-2-et00361712-1689772587.jpg&quot;);">flex
